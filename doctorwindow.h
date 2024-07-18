81e2f7df5dbae2ba98fe419dce88c0553d014146
kerolos601 <kirolos_adel@aucegypt.edu>
2024-07-19 00:25:30 +0300

    Add files via upload

--- doctorwindow.h
+++ doctorwindow.h
@@ -2,6 +2,7 @@
 #define DOCTORWINDOW_H
 
 #include <QDialog>
+#include <QTableWidgetItem>
 
 namespace Ui {
 class DoctorWindow;
@@ -15,8 +16,33 @@ public:
     explicit DoctorWindow(QWidget *parent = nullptr);
     ~DoctorWindow();
 
+private slots:
+    void on_addSlotButton_clicked();
+    void on_editSlotButton_clicked();
+    void on_deleteSlotButton_clicked();
+    void on_searchMedicalRecords_textChanged(const QString &text); // New slot for search
+
 private:
     Ui::DoctorWindow *ui;
+
+    // Initialize the tables
+    void initializeAppointmentsTable();
+    void initializeMedicalRecordsTable();
+    void initializeSlotsTable();
+
+    // Generate a random slot ID
+    int generateRandomSlotId();
+
+    // Save and load slots data from a file
+    void saveSlotsToFile(const QString& filename);
+    void loadSlotsFromFile(const QString& filename);
+
+    // Save and load medical records data from a file
+    void saveMedicalRecordsToFile(const QString& filename);
+    void loadMedicalRecordsFromFile(const QString& filename);
+
+    // Filter the medical records table based on search text
+    void filterMedicalRecordsTable(const QString &searchText);
 };
 
 #endif // DOCTORWINDOW_H
